74e3b423e6c5deb2073e60024f04c3bceaa033a5
Joey Trapp <joey@loadsys.com>
2014-06-11 11:22:58 -0500

    Added autorunning ruby tests with guard and minitest

---
+++ ruby/lib/demo.rb
@@ -0,0 +1,5 @@
+class Demo
+  def method
+    'demo response'
+  end
+end
---
+++ ruby/spec/demo_spec.rb
@@ -0,0 +1,7 @@
+require 'demo'
+
+describe Demo do
+  it '#method' do
+    assert_equal Demo.new.method, 'demo response'
+  end
+end
